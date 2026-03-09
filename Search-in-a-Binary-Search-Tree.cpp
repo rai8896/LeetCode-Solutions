80f408dd1ecdec1cf512aef695023a8b844267f7
Manish Rai <112763542+rai8896@users.noreply.github.com>
2026-03-09 19:57:30 +0530

    [700] [Time Beats: 100.00%] [Memory Beats: 63.00%] - LeetPush

---
+++ Search-in-a-Binary-Search-Tree.cpp
@@ -0,0 +1,30 @@
+1/**
+2 * Definition for a binary tree node.
+3 * struct TreeNode {
+4 *     int val;
+5 *     TreeNode *left;
+6 *     TreeNode *right;
+7 *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+8 *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+9 *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+10 * };
+11 */
+12class Solution {
+13public:
+14    TreeNode* searchBST(TreeNode* root, int val) {
+15
+16
+17        if(root==NULL || root->val==val)
+18        {
+19            return root;
+20        }
+21        if(val<root->val)
+22        {
+23            return searchBST(root->left,val);
+24        }else
+25        {
+26             return searchBST(root->right,val);
+27        }
+28        
+29    }
+30};
